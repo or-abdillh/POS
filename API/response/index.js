64a8308b1b1d0eee12067f260a288e71cec0ff7d
or-abdillh <hans.abdillh05@gmail.com>
2021-09-09 12:55:59 +0800

    Upgrade code to more better

--- API/response/index.js
+++ API/response/index.js
@@ -1,13 +1,23 @@
 'use strict';
 
+const setData = (code, message, values) => {
+   return {
+    'code': code,
+    'message': message,
+    'results': values  
+   }
+} 
+
+const setResponse = (data, res) => {
+   res.header('Access-Control-Allow-Origin', '*')
+   res.json(data);
+   res.end();
+}
+
 module.exports.success = (values, res) => {
-  const data = {
-    'code': 200,
-    'message': 'Success',
-    'results': values
-  };
-  
-  res.header('Access-Control-Allow-Origin', '*')
-  res.json(data);
-  res.end();
+  setResponse(setData(200, 'success' ,values), res);
+}
+
+module.exports.noContent = (res) => {
+   setResponse(setData(204, 'no content', []), res);
 }
